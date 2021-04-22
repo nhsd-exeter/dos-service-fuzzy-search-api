a952605f839989a168cf54c9a1984f265ab2476f
j.pearce1 <j.pearce1@nhs.net>
2021-04-22 12:36:43 +0100

    Fixed merging issues

--- build/automation/var/profile/dev.mk
+++ build/automation/var/profile/dev.mk
@@ -16,7 +16,7 @@ APP_URL_PREFIX := $(K8S_APP_NAMESPACE)-$(PROJECT_GROUP_SHORT)-$(PROJECT_NAME_SHO
 # Datastore URLs
 ELASTICSEARCH_URL := https://vpc-sfs-dev-csfa7ah6cfpbllxcfqf6wmdcxi.eu-west-2.es.amazonaws.com
 POSTCODE_LOCATION_DYNAMO_URL := https://dynamodb.eu-west-2.amazonaws.com
-DYNAMODB_POSTCODE_LOC_MAP_TABLE := service-finder-${PROFILE}-postcode-location-mapping
+DYNAMODB_POSTCODE_LOC_MAP_TABLE := service-finder-nonprod-postcode-location-mapping
 
 API_SERVICE_SEARCH_ENDPOINT := https://$(APP_URL_PREFIX)-service-search.$(TF_VAR_platform_zone)/dosapi/dosservices/v0.0.1/services/byfuzzysearch
 
@@ -38,21 +38,6 @@ NAME_PRIORITY := 4
 ADDRESS_PRIORITY := 2
 POSTCODE_PRIORITY := 0
 NAME_PUBLIC_PRIORITY := 4
-
-SPRING_PROFILES_ACTIVE := local
-CERTIFICATE_DOMAIN := localhost
-
-# Validation parameters
-MIN_SEARCH_TERM_LENGTH := 3
-MAX_SEARCH_CRITERIA := 5
-
-# Search parameters
-MAX_NUM_SERVICES_TO_RETURN := 5
-FUZZ_LEVEL := 0
-NAME_PRIORITY := 1
-ADDRESS_PRIORITY := 0
-POSTCODE_PRIORITY := 0
-PUBLIC_NAME_PRIORITY := 0
 
 # ==============================================================================
 # Infrastructure variables
