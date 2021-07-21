3d2bca5d6f58a9cac2964f4455d2ec7cc4341e7d
Arif Mohammed <81620747+ArifNhs@users.noreply.github.com>
2021-07-21 16:47:55 +0100

    SFD 3177 implement local authorisation (#46)
    * task/SFD-3177 authorising fuzzy search for local stub
    
    * task/SFD-3177 authentication exception handling
    
    * task/SFD-3177 more exception handling changes
    
    * task/SFD-3177 default message included for exception handling
    
    * task/SFD-3177 code cleanup
    
    * task/SFD-3177 ReadMe details added

--- application/app/src/main/java/uk/nhs/digital/uec/api/authentication/config/SecurityConfig.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/authentication/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package uk.nhs.digital.uec.api.authentication.config;
 
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import uk.nhs.digital.uec.api.authentication.filter.AccessTokenFilter;
+import uk.nhs.digital.uec.api.authentication.filter.TokenEntryPoint;
 
 /** Configuration class to further secure all the APIs */
 @Configuration
@@ -18,12 +23,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   public static final String WELCOME_URL = "/dosapi/dosservices/v0.0.1/home";
   public static final String FUZZY_SEARCH_URL = "/dosapi/dosservices/v0.0.1/services/byfuzzysearch";
 
+  @Autowired private AccessTokenFilter accessTokenFilter;
+  @Autowired private TokenEntryPoint tokenEndpoint;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-    List<String> permitAllEndpointList = Arrays.asList(LOGIN_URL, WELCOME_URL, FUZZY_SEARCH_URL);
+    List<String> permitAllEndpointList = Arrays.asList(LOGIN_URL, WELCOME_URL);
 
-    http.cors()
+    http.addFilterBefore(accessTokenFilter, AbstractPreAuthenticatedProcessingFilter.class)
+        .cors()
         .and()
         .csrf()
         .disable()
@@ -33,7 +42,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .anyRequest()
         .authenticated()
         .and()
-        .csrf()
-        .disable();
+        .exceptionHandling()
+        .authenticationEntryPoint(tokenEndpoint)
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 }
---
+++ application/app/src/main/java/uk/nhs/digital/uec/api/authentication/exception/AccessTokenExpiredException.java
@@ -0,0 +1,4 @@
+package uk.nhs.digital.uec.api.authentication.exception;
+
+/** Represents the detection of an expired access token */
+public class AccessTokenExpiredException extends Exception {}
