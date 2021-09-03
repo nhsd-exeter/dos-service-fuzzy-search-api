040fc9f76976a46ae385910b2e1c0010042a86c2
Arif Mohammed <81620747+ArifNhs@users.noreply.github.com>
2021-09-03 11:35:24 +0100

    SFD-3360 Update deployment attribute and removal of dynamo db re… (#62)
    * task/SFD-3360 Update deployment attribute and removal of dynamo db references for postcode mapping
    
    * task/SFD-3360 Application yaml attributes set right
    
    * task/SFD-3360 Application yaml dynamo attributes removed
    
    * task/SFD-3360 Application yaml whole dynamo section removed

--- data/dynamo/00-postcode-ccg-location-table.sh
+++
@@ -1,5 +0,0 @@
-aws dynamodb create-table --table-name ${DYNAMODB_POSTCODE_LOC_MAP_TABLE} \
-                    --attribute-definitions AttributeName=postcode,AttributeType=S \
-                    --key-schema AttributeName=postcode,KeyType=HASH \
-                    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
-                    --endpoint-url http://localhost:8000
