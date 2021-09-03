e03b618039967569fa1359dac1031704d99d3f31
Jonathan Pearce <j.pearce1@nhs.net>
2021-09-03 12:48:02 +0100

    Add authorisation to endpoints (#63)

--- application/app/src/main/java/uk/nhs/digital/uec/api/controller/FuzzyServiceSearchController.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/controller/FuzzyServiceSearchController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +51,7 @@ public class FuzzyServiceSearchController {
    */
   @GetMapping("services/byfuzzysearch")
   @CrossOrigin(origins = "*")
+  @PreAuthorize("hasAnyRole('FUZZY')")
   public ResponseEntity<ApiResponse> getServicesByFuzzySearch(
       @RequestParam(name = "search_term", required = false) List<String> searchCriteria,
       @RequestParam(name = "search_location", required = false) String searchPostcode,
