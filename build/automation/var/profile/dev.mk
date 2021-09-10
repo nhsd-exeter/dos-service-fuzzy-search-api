15906889844ce7bf9b472536167af630ab9efe35
Geoff <59701559+gwaardenburg@users.noreply.github.com>
2021-09-10 11:31:25 +0100

    SFD-3540 Increase Instance Of Elasticsearch (#65)
    * SFD-3540 Increase Instance Of Elasticsearch
    
    * reduced instance to 2

--- build/automation/var/profile/dev.mk
+++ build/automation/var/profile/dev.mk
@@ -50,8 +50,8 @@ TF_VAR_service_prefix_short := sfs-$(PROFILE)
 
 TF_VAR_es_zone_awareness_enabled := false
 TF_VAR_es_availability_zone_count := null
-TF_VAR_es_instance_count := 1
-TF_VAR_es_instance_type := t3.small.elasticsearch
+TF_VAR_es_instance_count := 2
+TF_VAR_es_instance_type := m4.large.elasticsearch
 TF_VAR_es_snapshot_bucket := $(TF_VAR_service_prefix)-elastic-search-snapshots
 TF_VAR_es_snapshot_role := $(TF_VAR_service_prefix)-elasticsearch-snapshot
 TF_VAR_es_domain_name := sfs-$(PROFILE)
