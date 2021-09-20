5602283a1e4309f723169e3f4c9ca28ab1bfce32
Jonathan Pearce <j.pearce1@nhs.net>
2021-09-20 12:11:37 +0100

    Fix local running

--- build/automation/var/profile/local.mk
+++ build/automation/var/profile/local.mk
@@ -7,7 +7,7 @@ CERTIFICATE_DOMAIN := localhost
 ALLOWED_ORIGINS := *
 
 # Datastore URLs
-ELASTICSEARCH_URL := elasticsearch.sfs.local:19200
+ELASTICSEARCH_URL := elasticsearch.sfs.local:9200
 API_SERVICE_SEARCH_ENDPOINT := https://localhost:8443/dosapi/dosservices/v0.0.1/services/byfuzzysearch
 
 SERVER_PORT := 8443
