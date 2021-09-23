bc8854a0b6d1c840398e39494d479e821173ec12
daonhs <daniel.osae@nhs.net>
2021-09-23 14:52:45 +0100

    SFD-3547 seperate authentication service and integration tests

--- application/authentication/src/main/java/uk/nhs/digital/uec/api/utils/Constants.java
+++ application/authentication/src/main/java/uk/nhs/digital/uec/api/utils/Constants.java
@@ -18,7 +18,7 @@ public class Constants {
 
   public static final String ROLE_PREFIX = "ROLE_";
 
-  public static final String KEY_STORE = "integTest.jks";
+  public static final String KEY_STORE = "integTestRSA.jks";
 
   public static final String KEY_STORE_PASSWORD = "mypass";
 
