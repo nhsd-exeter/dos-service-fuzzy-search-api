646f3500d20a1231941d4a72c04ae06907508725
Jonathan Pearce <j.pearce1@nhs.net>
2021-10-06 10:38:18 +0100

    Add https to front of URL for elasticsearch (#83)

--- build/automation/lib/project.mk
+++ build/automation/lib/project.mk
@@ -57,7 +57,8 @@ project-populate-application-variables:
 	export COGNITO_USER_POOL_ID=$$(make -s aws-cognito-get-userpool-id NAME=$(COGNITO_USER_POOL))
 	export COGNITO_JWT_VERIFICATION_URL=https://cognito-idp.eu-west-2.amazonaws.com/$${COGNITO_USER_POOL_ID}/.well-known/jwks.json
 
-	export ELASTICSEARCH_URL=$$(make aws-elasticsearch-get-endpoint DOMAIN=$(DOMAIN))
+	export ELASTICSEARCH_EP=$$(make aws-elasticsearch-get-endpoint DOMAIN=$(DOMAIN))
+	export ELASTICSEARCH_URL=https://$${ELASTICSEARCH_EP}
 
 project-aws-get-cognito-client-id: # Get AWS cognito client id - mandatory: NAME
 	aws cognito-idp list-user-pool-clients \
