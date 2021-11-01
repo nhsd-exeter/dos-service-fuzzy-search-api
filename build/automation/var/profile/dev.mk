e306feaa9921427b1d14c0202ce95479e3b861c3
Geoff <59701559+gwaardenburg@users.noreply.github.com>
2021-11-01 11:01:49 +0000

    SFD-3747 Remove Any References To DynamoDb (#101)
    * SFD-3747 Remove Any References To DynamoDB
    
    * SFD-3747 Tidied Up Folder Structure From Previous Commit

--- build/automation/var/profile/dev.mk
+++ build/automation/var/profile/dev.mk
@@ -46,9 +46,8 @@ NAME_PUBLIC_PRIORITY := 4
 # Infrastructure variables
 
 DEPLOYMENT_STACKS = application
-INFRASTRUCTURE_STACKS = elasticsearch,authentication,roles,service_etl
+INFRASTRUCTURE_STACKS = elasticsearch,authentication,service_etl
 SERVICE_PREFIX := $(PROJECT_GROUP_SHORT)-$(PROJECT_NAME_SHORT)-$(ENVIRONMENT)
-TF_VAR_dynamo_db_postcode_store_table_name = $(PROJECT_GROUP_SHORT)-pc-$(PROFILE)-postcode-location-mapping
 TF_VAR_service_prefix := $(PROJECT_GROUP_SHORT)-$(PROJECT_NAME_SHORT)-$(ENVIRONMENT)
 
 TF_VAR_es_zone_awareness_enabled := false
--- infrastructure/stacks/roles/data.tf
+++
@@ -1,3 +0,0 @@
-data "aws_dynamodb_table" "postcode_store" {
-  name = local.postcode_store_name
-}
