0f60bea55455c26669c52e137efcd5c1a202ab5e
Arif <arif.mohammed2@nhs.net>
2021-11-26 11:42:48 +0000

    SFD-3827 Swagger documentation changes included

--- application/app/src/main/java/uk/nhs/digital/uec/api/authentication/config/SecurityConfig.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/authentication/config/SecurityConfig.java
@@ -25,13 +25,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   public static final String WELCOME_URL = "/dosapi/dosservices/v0.0.1/home";
   public static final String FUZZY_SEARCH_URL = "/dosapi/dosservices/v0.0.1/services/byfuzzysearch";
 
+  public static final String SWAGGER_URL = "/swagger-ui.html";
+  public static final String SWAGGER_API_DOCS = "/v2/api-docs";
+  public static final String SWAGGER_RESOURCES_DIR = "/swagger-resources/**";
+  public static final String SWAGGER_WEBJARS = "/webjars/**";
+  public static final String SWAGGER_API_DOCS_DIR = "/v3/api-docs/**";
+  public static final String SWAGGER_UI_DIR = "/swagger-ui/**";
+
   @Autowired private AccessTokenFilter accessTokenFilter;
   @Autowired private TokenEntryPoint tokenEndpoint;
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-    List<String> permitAllEndpointList = Arrays.asList(LOGIN_URL, HEALTH_CHECK_READINESS_URL,HEALTH_CHECK_LIVENESS_URL);
+    List<String> permitAllEndpointList =
+        Arrays.asList(
+            WELCOME_URL,
+            FUZZY_SEARCH_URL,
+            LOGIN_URL,
+            HEALTH_CHECK_READINESS_URL,
+            HEALTH_CHECK_LIVENESS_URL,
+            SWAGGER_URL,
+            SWAGGER_API_DOCS,
+            SWAGGER_RESOURCES_DIR,
+            SWAGGER_WEBJARS,
+            SWAGGER_API_DOCS_DIR,
+            SWAGGER_UI_DIR);
 
     http.addFilterBefore(accessTokenFilter, AbstractPreAuthenticatedProcessingFilter.class)
         .cors()
---
+++ application/app/src/main/java/uk/nhs/digital/uec/api/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package uk.nhs.digital.uec.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Value("${api.version}")
+  private String apiVersion;
+
+  @Value("${api.title}")
+  private String title;
+
+  @Value("${api.description}")
+  private String description;
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  ApiInfo apiInfo() {
+    return new ApiInfoBuilder().title(title).description(description).version(apiVersion).build();
+  }
+}
