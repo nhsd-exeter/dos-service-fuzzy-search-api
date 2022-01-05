ac162a9cfb632b483f4524f00ab63b16d74efb93
Arif Mohammed <81620747+ArifNhs@users.noreply.github.com>
2022-01-05 12:53:05 +0000

    SFD-3974 Junit test profile created (#114)

---
+++ build/automation/var/profile/test.mk
@@ -0,0 +1,32 @@
+include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
+
+# ==============================================================================
+# Service variables
+SPRING_PROFILES_ACTIVE := local
+CERTIFICATE_DOMAIN := localhost
+ALLOWED_ORIGINS := *
+
+# Datastore URLs
+ELASTICSEARCH_URL := elasticsearch.sfs.local:9200
+API_SERVICE_SEARCH_ENDPOINT := https://localhost:8443/dosapi/dosservices/v0.0.1/services/byfuzzysearch
+
+SERVER_PORT := 8443
+VERSION := v0.0.3
+
+# Validation parameters
+MIN_SEARCH_TERM_LENGTH := 3
+MAX_SEARCH_CRITERIA := 5
+
+# Search parameters
+MAX_NUM_SERVICES_TO_RETURN_FROM_ELASTICSEARCH := 100
+MAX_NUM_SERVICES_TO_RETURN_FROM_ELASTICSEARCH_3_SEARCH_TERMS := 50
+MAX_NUM_SERVICES_TO_RETURN := 5
+FUZZ_LEVEL := 0
+NAME_PRIORITY := 1
+ADDRESS_PRIORITY := 0
+POSTCODE_PRIORITY := 0
+NAME_PUBLIC_PRIORITY := 0
+
+# Service Data files
+SERVICE_DATA_FILE := create_all_services_local.sh
+LOCATIONS_DATA_FILE := load_locations_test.sh
