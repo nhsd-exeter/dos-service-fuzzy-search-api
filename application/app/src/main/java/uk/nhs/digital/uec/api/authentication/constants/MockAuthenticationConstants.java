f152d21232b69d75ab11ed440a11c3f6bdcafb73
Arif <arif.mohammed2@nhs.net>
2022-01-17 10:50:19 +0000

    SFD-4005 Review comments changes included

--- application/app/src/main/java/uk/nhs/digital/uec/api/authentication/constants/MockAuthenticationConstants.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/authentication/constants/MockAuthenticationConstants.java
@@ -4,6 +4,6 @@ public class MockAuthenticationConstants {
 
   private MockAuthenticationConstants() {}
 
-  public static final String MOCK_AUTH_GROUP = "POSTCODE_API_ACCESS";
-  public static final String MOCK_ACCESS_TOKEN = "MOCK_POSTCODE_API_ACCESS_TOKEN";
+  public static final String MOCK_AUTH_GROUP = "FUZZY_API_ACCESS";
+  public static final String MOCK_ACCESS_TOKEN = "MOCK_FUZZY_API_ACCESS_TOKEN";
 }
