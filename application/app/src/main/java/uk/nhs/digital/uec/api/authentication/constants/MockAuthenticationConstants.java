0f0408a4cbc8634529c99f529fb7f7ccd2ad27ea
Arif <arif.mohammed2@nhs.net>
2022-03-04 12:41:23 +0000

    SFD-4208 Authentication from fuzzy to postcode for non prod env

--- application/app/src/main/java/uk/nhs/digital/uec/api/authentication/constants/MockAuthenticationConstants.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/authentication/constants/MockAuthenticationConstants.java
@@ -6,4 +6,5 @@ public class MockAuthenticationConstants {
 
   public static final String MOCK_AUTH_GROUP = "FUZZY_API_ACCESS";
   public static final String MOCK_ACCESS_TOKEN = "MOCK_FUZZY_API_ACCESS_TOKEN";
+  public static final String MOCK_PCA_ACCESS_TOKEN = "MOCK_POSTCODE_API_ACCESS_TOKEN";
 }
