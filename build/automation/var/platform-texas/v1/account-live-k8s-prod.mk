fea0ec1f14ff024446c0358e5ac4015679440c12
Daniel Osae <86474960+daonhs@users.noreply.github.com>
2022-03-09 13:56:09 +0000

    SFD-1234 Tweaks to infrastructure code (#140)

--- build/automation/var/platform-texas/v1/account-live-k8s-prod.mk
+++ build/automation/var/platform-texas/v1/account-live-k8s-prod.mk
@@ -9,6 +9,14 @@ TF_VAR_terraform_platform_state_store = nhsd-texasplatform-terraform-state-store
 # ==============================================================================
 
 
+# TODO - Point to put for now, but this will need to be split into demo and live profiles
+# Connection to DoS Read Replica for extraction Lambdas
+TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-replica-sf.dos-db-rds
+TF_VAR_service_finder_replica_sg := uec-core-dos-put-db-12-replica-sf-sg
+TF_VAR_dos_read_replica_secret_name := core-dos/deployment
+TF_VAR_dos_read_replica_secret_key := DB_SF_READONLY_PASSWORD
+TF_VAR_service_etl_db_user := dos_sf_readonly
+TF_VAR_service_etl_source_db := pathwaysdos
 TF_VAR_core_dos_python_libs := core-dos-python-libs
 
 # ==============================================================================
