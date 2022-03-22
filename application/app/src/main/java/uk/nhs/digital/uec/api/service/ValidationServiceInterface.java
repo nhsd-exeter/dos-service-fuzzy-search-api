e55597fbe3fd7067ccf028cef0068acef12f0e52
Arif Mohammed <81620747+ArifNhs@users.noreply.github.com>
2022-03-22 18:03:31 +0000

    SFD-4250 Return success if no search data found (#148)
    * SFD-4250 Return success if no search data found
    
    * SFD-4250 Send success when search data is not found

--- application/app/src/main/java/uk/nhs/digital/uec/api/service/ValidationServiceInterface.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/service/ValidationServiceInterface.java
@@ -2,7 +2,6 @@ package uk.nhs.digital.uec.api.service;
 
 import java.util.List;
 import uk.nhs.digital.uec.api.exception.NotFoundException;
-import uk.nhs.digital.uec.api.model.DosService;
 
 /** Interface encapsulating validation processes used by the API. */
 public interface ValidationServiceInterface {
@@ -16,12 +15,4 @@ public interface ValidationServiceInterface {
    * @throws NotFoundException when business requirements are not met.
    */
   public void validateSearchCriteria(final List<String> searchCriteria) throws NotFoundException;
-
-  /**
-   * Validates if there are any dos services returned
-   *
-   * @param dosService the searchCriteria string to validate.
-   * @throws NotFoundException when none of the dos search results are returned
-   */
-  public void validateDosService(List<DosService> dosServices) throws NotFoundException;
 }
