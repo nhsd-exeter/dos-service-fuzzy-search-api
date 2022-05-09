6aee80fb872fffa1ecc0f754812f8345c8108daf
adeniyiabdul <adeniyi.abdul1@nhs.net>
2022-05-09 18:20:48 +0100

    SFD-XXXX Updated prod certificate

--- build/automation/var/platform-texas/v1/account-live-k8s-prod.mk
+++ build/automation/var/platform-texas/v1/account-live-k8s-prod.mk
@@ -1,7 +1,7 @@
 AWS_ACCOUNT_ID = $(AWS_ACCOUNT_ID_PROD)
 AWS_ACCOUNT_NAME = prod
 
-TEXAS_CERTIFICATE_ID = 8d65eee4-cf92-4a00-84de-7a9f544ba724
+TEXAS_CERTIFICATE_ID = 8b67daa2-2b82-4287-b925-d74ab9fa68ce
 TEXAS_WAF_ACL_ID = ff530a4e-689d-4d90-b3ab-ac2160b5863d
 
 TF_VAR_terraform_platform_state_store = nhsd-texasplatform-terraform-state-store-lk8s-$(AWS_ACCOUNT_NAME)
