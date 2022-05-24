ad9352967e322ee454ef31cdd04d870f9af32364
adeniyiabdul <99728546+adeniyiabdul@users.noreply.github.com>
2022-05-24 17:06:22 +0100

    Updated files for new changes

--- build/automation/var/platform-texas/v1/account-live-k8s-prod.mk
+++ build/automation/var/platform-texas/v1/account-live-k8s-prod.mk
@@ -5,7 +5,10 @@ TEXAS_CERTIFICATE_ID = 8b67daa2-2b82-4287-b925-d74ab9fa68ce
 TEXAS_WAF_ACL_ID = ff530a4e-689d-4d90-b3ab-ac2160b5863d
 
 TF_VAR_terraform_platform_state_store = nhsd-texasplatform-terraform-state-store-lk8s-$(AWS_ACCOUNT_NAME)
-
+TF_VAR_project_id = $(PROJECT_ID)
+TF_VAR_eks_terraform_state_key = eks/terraform.tfstate
+TF_VAR_aws_account_id = $(AWS_ACCOUNT_ID_PROD)
+TF_VAR_application_service_account_name = $(APPLICATION_SA_NAME)
 # ==============================================================================
 
 
