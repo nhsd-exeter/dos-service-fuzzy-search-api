e1517d376ecddba3e7e57b5242c78047bb0636c6
adeniyiabdul <adeniyi.abdul1@nhs.net>
2022-05-30 19:39:31 +0100

    Updated the security group for read replica

--- build/automation/var/profile/stg.mk
+++ build/automation/var/profile/stg.mk
@@ -88,7 +88,7 @@ TF_VAR_service_etl_alarm_period := 240
 
 # Connection to DoS Read Replica for extraction Lambdas. For the Demo env we point to the live read replica
 TF_VAR_dos_sf_replica_db := uec-core-dos-regression-db-12-replica-sf
-TF_VAR_service_finder_replica_sg := live-lk8s-nonprod-uec-sf-core-dos-sf-replica-s
+TF_VAR_service_finder_replica_sg := live-lk8s-nonprod-uec-sf-core-dos-sf-replica-sg
 TF_VAR_dos_read_replica_secret_name := core-dos-dev/deployment
 # TF_VAR_dos_read_replica_secret_key := DB_USER_PASSWORD
 TF_VAR_dos_read_replica_secret_key := DB_SF_READONLY_PASSWORD
