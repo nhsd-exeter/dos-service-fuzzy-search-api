abf11adb9a1d53e44c362ceddd1d59990d633f08
adeniyiabdul <99728546+adeniyiabdul@users.noreply.github.com>
2022-05-30 14:19:33 +0100

    Rename staging from sta to stg (#201)

--- build/automation/var/profile/stg.mk
+++ build/automation/var/profile/stg.mk
@@ -5,7 +5,7 @@ include $(VAR_DIR)/platform-texas/v1/account-live-k8s-nonprod.mk
 AWS_CERTIFICATE := arn:aws:acm:eu-west-2:$(AWS_ACCOUNT_ID):certificate/c0718115-4e22-4f48-a4aa-8c16ea86c5e6
 ECR_TEXAS_URL_NONPROD = $(AWS_ECR_NON_PROD)/texas
 
-PROFILE := sta
+PROFILE := stg
 ENVIRONMENT := $(PROFILE)
 SPRING_PROFILES_ACTIVE := $(PROFILE)
 API_IMAGE_TAG := v0.0.3
