ebc56c125748a1bbdb4501ebe818829a127f854e
adeniyiabdul <adeniyi.abdul1@nhs.net>
2022-05-31 17:46:49 +0100

    Updated the read replica variables for dmo env 2

--- build/automation/var/profile/dmo.mk
+++ build/automation/var/profile/dmo.mk
@@ -96,8 +96,8 @@ TF_VAR_service_etl_alarm_period := 240
 
 
 # Connection to DoS Read Replica for extraction Lambdas. For the Demo env we point to the live read replica
-TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf.crvqtzolulpo.eu-west-2.rds.amazonaws.com
-#TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf
+#TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf.crvqtzolulpo.eu-west-2.rds.amazonaws.com
+TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf
 TF_VAR_service_finder_replica_sg := uec-core-dos-put-db-12-replica-sf-sg
 TF_VAR_dos_read_replica_secret_name := core-dos-uet-database-upgrade/deployment
 TF_VAR_dos_read_replica_secret_key := DB_SF_READONLY_PASSWORD
