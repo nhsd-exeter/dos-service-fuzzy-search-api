6a6b3260bfabdb0591381821bbf0ed63354a739a
Daniel Osae <86474960+daonhs@users.noreply.github.com>
2022-06-20 11:54:46 +0100

    SFD-XXXX Update postcode URL for fuzzy (#228)

--- build/automation/var/profile/pt.mk
+++ build/automation/var/profile/pt.mk
@@ -92,7 +92,7 @@ COGNITO_USER_POOL_CLIENT_ID := $(or $(COGNITO_USER_POOL_CLIENT_ID), )
 COGNITO_USER_POOL_ID := $(or $(COGNITO_USER_POOL_ID), )
 ADD_DEFAULT_COGNITO_USERS := false
 
-POSTCODE_MAPPING_SERVICE_URL := https://uec-dos-api-pca-dev-uec-dos-api-pc-ingress.k8s-nonprod.texasplatform.uk/api
+POSTCODE_MAPPING_SERVICE_URL := https://uec-dos-api-pc-dev-uec-dos-api-pc-ingress.k8s-nonprod.texasplatform.uk/api
 #Once wiremock is deployed to dev environment calls to postcode api will be mocked
 # POSTCODE_MAPPING_SERVICE_URL := http://mockservice.sfs.test:8080/api
 
