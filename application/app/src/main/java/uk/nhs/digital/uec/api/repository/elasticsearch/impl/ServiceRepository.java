74f80028c9ff5bba808f97254209b573f3195b8e
daniel osae <daniel.osae@nhs.net>
2022-06-23 14:29:45 +0100

    SFD-4532 Add more results to returned list

--- application/app/src/main/java/uk/nhs/digital/uec/api/repository/elasticsearch/impl/ServiceRepository.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/repository/elasticsearch/impl/ServiceRepository.java
@@ -1,8 +1,5 @@
 package uk.nhs.digital.uec.api.repository.elasticsearch.impl;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +10,32 @@ import uk.nhs.digital.uec.api.model.DosService;
 import uk.nhs.digital.uec.api.repository.elasticsearch.CustomServicesRepositoryInterface;
 import uk.nhs.digital.uec.api.repository.elasticsearch.ServicesRepositoryInterface;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+
 @Repository
 @Slf4j
 public class ServiceRepository implements CustomServicesRepositoryInterface {
 
-  @Autowired private ServicesRepositoryInterface servicesRepo;
+  @Autowired
+  private ServicesRepositoryInterface servicesRepo;
 
-  @Autowired private ApiRequestParams apiRequestParams;
+  @Autowired
+  private ApiRequestParams apiRequestParams;
 
-  /** {@inheritDoc} */
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public List<DosService> findServiceBySearchTerms(List<String> searchTerms) {
     final List<DosService> dosServices = new ArrayList<>();
 
     log.info("Request Params: " + apiRequestParams.getFuzzLevel());
     log.info(
-        "Number of services to get from elasticsearch: "
-            + apiRequestParams.getMaxNumServicesToReturnFromElasticsearch());
+      "Number of services to get from elasticsearch: "
+        + apiRequestParams.getMaxNumServicesToReturnFromElasticsearch());
 
     // Flattern search term list
     String searchCriteria = String.join(" ", searchTerms);
@@ -40,29 +46,32 @@ public class ServiceRepository implements CustomServicesRepositoryInterface {
     int numServicesToReturnFromEs = apiRequestParams.getMaxNumServicesToReturnFromElasticsearch();
     if (numOfSpaces == 2) {
       numServicesToReturnFromEs =
-          apiRequestParams.getMaxNumServicesToReturnFromElasticsearch3SearchTerms();
+        apiRequestParams.getMaxNumServicesToReturnFromElasticsearch3SearchTerms();
     }
     if (numOfSpaces > 2) {
       numServicesToReturnFromEs =
-          Math.floorDiv(
-              apiRequestParams.getMaxNumServicesToReturnFromElasticsearch3SearchTerms(), 2);
+        Math.floorDiv(
+          apiRequestParams.getMaxNumServicesToReturnFromElasticsearch3SearchTerms(), 2);
     }
 
     Long start = System.currentTimeMillis();
     Iterable<DosService> services =
-        servicesRepo.findBySearchTerms(
-            searchCriteria,
-            apiRequestParams.getFuzzLevel(),
-            apiRequestParams.getNamePriority(),
-            apiRequestParams.getAddressPriority(),
-            apiRequestParams.getPostcodePriority(),
-            apiRequestParams.getPublicNamePriority(),
-            PageRequest.of(0, numServicesToReturnFromEs));
+      servicesRepo.findBySearchTerms(
+        searchCriteria,
+        apiRequestParams.getFuzzLevel(),
+        apiRequestParams.getNamePriority(),
+        apiRequestParams.getAddressPriority(),
+        apiRequestParams.getPostcodePriority(),
+        apiRequestParams.getPublicNamePriority(),
+        PageRequest.of(0, numServicesToReturnFromEs));
     log.info("Search query duration {}ms", System.currentTimeMillis() - start);
 
     Iterator<DosService> serviceit = services.iterator();
     while (serviceit.hasNext()) {
-      dosServices.add(serviceit.next());
+      DosService serviceIterationItem = serviceit.next();
+      if (Objects.nonNull(serviceIterationItem.getReferral_roles()) && !serviceIterationItem.getReferral_roles().isEmpty()) {
+        dosServices.add(serviceIterationItem);
+      }
     }
 
     return dosServices;
