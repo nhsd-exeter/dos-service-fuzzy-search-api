5bddda1d4a1ba00498e74748a84ab2b229664cdc
daniel osae <daniel.osae@nhs.net>
2022-08-24 12:44:14 +0100

    SFD-4665 Update to service etl to display opening times

--- infrastructure/stacks/service_etl/functions/service_etl/service_etl.py
+++ infrastructure/stacks/service_etl/functions/service_etl/service_etl.py
@@ -202,20 +202,20 @@ def connect_to_elastic_search():
         service = 'es'
         credentials = boto3.Session().get_credentials()
         awsauth = AWS4Auth(credentials.access_key,
-                        credentials.secret_key,
-                        region,
-                        service,
-                        session_token=credentials.token)
+                            credentials.secret_key,
+                            region,
+                            service,
+                            session_token=credentials.token)
 
         logger.debug("connecting to es")
         es = Elasticsearch(hosts=[{
             'host': host,
             'port': 443
         }],
-            http_auth=awsauth,
-            use_ssl=True,
-            verify_certs=True,
-            connection_class=RequestsHttpConnection)
+                            http_auth=awsauth,
+                            use_ssl=True,
+                            verify_certs=True,
+                            connection_class=RequestsHttpConnection)
 
         logger.debug(es.info())
         if not es.ping():
