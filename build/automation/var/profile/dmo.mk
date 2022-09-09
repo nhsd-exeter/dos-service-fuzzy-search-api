7ad3b1eb7ae7f7675dbda733420810dce42a770d
Daniel Osae <86474960+daonhs@users.noreply.github.com>
2022-09-09 10:53:54 +0100

    SFD-XXXX Use live data in demo for consitent testing (#261)

--- build/automation/var/profile/dmo.mk
+++ build/automation/var/profile/dmo.mk
@@ -94,13 +94,19 @@ TF_VAR_service_etl_alarm_period := 240
 
 
 # Connection to DoS Read Replica for extraction Lambdas. For the Demo env we point to the live read replica
-TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf.crvqtzolulpo.eu-west-2.rds.amazonaws.com
-#TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf
-TF_VAR_service_finder_replica_sg := uec-core-dos-put-db-12-replica-sf-sg
-TF_VAR_dos_read_replica_secret_name := core-dos-uet-database-upgrade/deployment
+# TF_VAR_dos_sf_replica_db := uec-core-dos-put-db-12-replica-sf.crvqtzolulpo.eu-west-2.rds.amazonaws.com
+# TF_VAR_service_finder_replica_sg := uec-core-dos-put-db-12-replica-sf-sg
+# TF_VAR_dos_read_replica_secret_name := core-dos-uet-database-upgrade/deployment
+# TF_VAR_dos_read_replica_secret_key := DB_SF_READONLY_PASSWORD
+# TF_VAR_service_etl_db_user := dos_sf_readonly
+# TF_VAR_service_etl_source_db := pathwaysdos_ut
+
+TF_VAR_dos_sf_replica_db := uec-core-dos-live-db-12-replica-sf.crvqtzolulpo.eu-west-2.rds.amazonaws.com #uec-core-dos-live-db-12-replica-sf.dos-db-rds
+TF_VAR_service_finder_replica_sg := uec-core-dos-live-db-12-replica-sf-sg
+TF_VAR_dos_read_replica_secret_name := core-dos/deployment
 TF_VAR_dos_read_replica_secret_key := DB_SF_READONLY_PASSWORD
 TF_VAR_service_etl_db_user := dos_sf_readonly
-TF_VAR_service_etl_source_db := pathwaysdos_ut
+TF_VAR_service_etl_source_db := pathwaysdos
 
 
 ####Cognito user pool details
