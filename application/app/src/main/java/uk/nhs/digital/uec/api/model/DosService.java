7abbaee77be2a396b7760aa54f869e5ffede5f1e
Rohit Kondapalli <rohit.kondapalli1@nhs.net>
2022-09-21 18:19:53 +0100

    Resolved referral filter issue

--- application/app/src/main/java/uk/nhs/digital/uec/api/model/DosService.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/model/DosService.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.decimal4j.util.DoubleRounder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 
 
@@ -23,19 +24,23 @@ public class DosService implements Comparable<DosService> {
 
   @Id private int id;
 
+  @Field(name="uIdentifier")
   private int u_id; // NOSONAR
 
   private String name;
 
+  @Field(name="publicName")
   private String public_name; // NOSONAR
 
+  @Field(name="capacityStatus")
   private String capacity_status; // NOSONAR
 
+  @Field(name="typeId")
   private int type_id; // NOSONAR
 
   private String type;
 
-  private String ods_code; // NOSONAR
+    private String ods_code; // NOSONAR
 
   private List<String> address;
 
@@ -45,22 +50,28 @@ public class DosService implements Comparable<DosService> {
 
   private Integer northing;
 
+  @Field(name="referralRoles")
   private List<String> referral_roles; // NOSONAR
 
   private Double distance;
 
+  @Field(name="publicPhoneNumber")
   private String public_phone_number;
 
+  @Field(name="nonPublicPhoneNumber")
   private String non_public_phone_number; // NOSONAR
 
   private String email;
 
   private String web;
 
+  @Field(name="publicReferralInstructions")
   private String public_referral_instructions;
 
+  @Field(name="referralInstructions")
   private String referral_instructions;
 
+  @Field(name="isNational")
   private String is_national;
 
   private String updated;
