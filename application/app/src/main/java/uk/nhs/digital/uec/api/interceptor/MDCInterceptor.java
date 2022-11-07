ec7b8b387fa4cac4b832b0078328360f41fff577
Rohit Kondapalli <rohit.kondapalli1@nhs.net>
2022-11-07 15:00:02 +0000

    Add configure correlationId

---
+++ application/app/src/main/java/uk/nhs/digital/uec/api/interceptor/MDCInterceptor.java
@@ -0,0 +1,55 @@
+package uk.nhs.digital.uec.api.interceptor;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.MDC;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class MDCInterceptor implements HandlerInterceptor {
+
+
+  private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
+  private static final String CORRELATION_ID_LOG_VAR_NAME = "correlationId";
+
+  @Override
+  public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
+          throws Exception {
+            final String correlationId = getCorrelationIdFromHeaderOrParams(request);
+            MDC.put(CORRELATION_ID_LOG_VAR_NAME, correlationId);
+            return true;
+  }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable Exception ex) throws Exception {
+              MDC.remove(CORRELATION_ID_LOG_VAR_NAME);
+    }
+
+
+    private String generateUniqueCorrelationId() {
+      return UUID.randomUUID().toString();
+    }
+
+    private String getCorrelationIdFromHeaderOrParams(final HttpServletRequest request) {
+      String correlationId = request.getParameter(CORRELATION_ID_HEADER_NAME);
+      if (!StringUtils.isBlank(correlationId)) {
+        log.info("received correlationId from service finder request params {}", correlationId);
+        return correlationId;
+      }
+      correlationId = request.getHeader(CORRELATION_ID_HEADER_NAME);
+      if (!StringUtils.isBlank(correlationId)) {
+        log.info("received correlationId from service finder request header {}", correlationId);
+        return correlationId;
+
+      }
+      log.info("generated new correlationId in fuzzy search {}", correlationId);
+      return generateUniqueCorrelationId();
+    }
+}
