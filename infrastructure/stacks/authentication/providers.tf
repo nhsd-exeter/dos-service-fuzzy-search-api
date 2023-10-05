e4efff89ea0dfda3b64273524ae28b9ba8fcfb51
Daniel Osae <daniel.osae@nhs.net>
2023-10-05 11:27:15 +0100

    SFD-XXXX Test new provider version for cognito

--- infrastructure/stacks/authentication/providers.tf
+++ infrastructure/stacks/authentication/providers.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
-  version = "4.20.0"
-  # version = ">= 3.74.1, < 4.0.0"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.20.0"
+      # or if you want to use a range
+      # version = ">= 3.74.1, < 4.0.0"
+    }
+  }
 }
