ceffada86bde1a862d0badcd62f9d9907c515497
Daniel Osae <daniel.osae@nhs.net>
2023-10-05 15:34:08 +0100

    SFD-XXXX Test new provider version for cognito

--- build/automation/lib/terraform.mk
+++ build/automation/lib/terraform.mk
@@ -49,10 +49,13 @@ terraform-import-stack:
 	if [[ ! "$(TERRAFORM_REINIT)" =~ ^(false|no|n|off|0|FALSE|NO|N|OFF)$$ ]] || [ ! -f $(TERRAFORM_DIR)/$(STACK)/terraform.tfstate ]; then
 		make _terraform-reinitialise DIR="$(TERRAFORM_DIR)" STACK="$(STACK)"
 	fi
-	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_iam_role.cw_to_subscription_filter_role uec-sf-sfsa-pd_CWLtoSubscriptionFilterRole" || true
-	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_iam_policy.central_cw_subscription_iam_policy uec-sf-sfsa-pd_central_cw_subscription" || true
-	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_wafv2_web_acl.waf_acl uec-sf-sfsa-pd-waf-acl" || true
-	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$(STACK)" CMD="import aws_cloudwatch_log_group.waf_logs aws-waf-logs-uec-sf-sfsa-pd" || true
+	STACK=firewall
+	echo $$STACK
+	make docker-run-terraform DIR="$(TERRAFORM_DIR)/$$STACK" CMD="import aws_iam_policy.central_cw_subscription_iam_policy arn:aws:iam::$(AWS_ACCOUNT_ID):policy/uec-sf-sfsa-pd_central_cw_subscription"
+	make docker-run-terraform DIR="$(TERRAFORM_DIR)/$$STACK" CMD="import aws_wafv2_web_acl.waf_acl 56aa1022-ec08-430d-936e-08266c7b92fb/uec-sf-sfsa-pd-waf-acl/REGIONAL"
+
+	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$$STACK" CMD="import aws_iam_role.cw_to_subscription_filter_role uec-sf-sfsa-pd_CWLtoSubscriptionFilterRole" || true
+	# make docker-run-terraform DIR="$(TERRAFORM_DIR)/$$STACK" CMD="import aws_cloudwatch_log_group.waf_logs aws-waf-logs-uec-sf-sfsa-pd" || true
 
 
 # ==============================================================================
