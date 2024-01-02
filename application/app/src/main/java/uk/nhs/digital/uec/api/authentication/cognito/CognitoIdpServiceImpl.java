325b8666d77608ac1c71f3b2c2b96ef8ad2b83e3
Rohit Kondapalli <rohit.kondapalli1@nhs.net>
2024-01-02 13:37:55 +0000

    Made few changes in code due to java 17 upgrade

--- application/app/src/main/java/uk/nhs/digital/uec/api/authentication/cognito/CognitoIdpServiceImpl.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/authentication/cognito/CognitoIdpServiceImpl.java
@@ -1,11 +1,20 @@
 package uk.nhs.digital.uec.api.authentication.cognito;
 
+import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.PASSWORD;
+import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.REFRESH_TOKEN;
+import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.REFRESH_TOKEN_AUTH;
+import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.USERNAME;
+import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.USER_PASSWORD_AUTH;
+import static uk.nhs.digital.uec.api.util.Utils.calculateSecretHash;
+
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.model.AWSCognitoIdentityProviderException;
 import com.amazonaws.services.cognitoidp.model.InitiateAuthRequest;
 import com.amazonaws.services.cognitoidp.model.InitiateAuthResult;
 import com.amazonaws.services.cognitoidp.model.InvalidPasswordException;
 import com.amazonaws.services.cognitoidp.model.NotAuthorizedException;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,23 +24,13 @@ import uk.nhs.digital.uec.api.authentication.exception.UnauthorisedException;
 import uk.nhs.digital.uec.api.authentication.model.AuthToken;
 import uk.nhs.digital.uec.api.authentication.model.Credential;
 
-import java.util.Map;
-
-import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.PASSWORD;
-import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.REFRESH_TOKEN;
-import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.REFRESH_TOKEN_AUTH;
-import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.USERNAME;
-import static uk.nhs.digital.uec.api.authentication.constants.AuthenticationConstants.USER_PASSWORD_AUTH;
-import static uk.nhs.digital.uec.api.util.Utils.calculateSecretHash;
-
 @Service
 @Slf4j
 public class CognitoIdpServiceImpl implements CognitoIdpService {
 
-  @Autowired
-  private AWSCognitoIdentityProvider cognitoClient;
-  @Autowired
-  private Environment environment;
+  @Autowired private AWSCognitoIdentityProvider cognitoClient;
+
+  @Autowired private Environment environment;
 
   @Value("${cognito.userPool.clientId}")
   private String userPoolClientId;
@@ -41,15 +40,13 @@ public class CognitoIdpServiceImpl implements CognitoIdpService {
 
   @Override
   public AuthToken authenticate(Credential credential) throws UnauthorisedException {
-    Map<String, String> authenticationParameters =
-      Map.of(
-        USERNAME,
-        credential.getEmailAddress(),
-        PASSWORD,
-        credential.getPassword(),
+    Map<String, String> authenticationParameters = new HashMap<>();
+    authenticationParameters.put(USERNAME, credential.getEmailAddress());
+    authenticationParameters.put(PASSWORD, credential.getPassword());
+    authenticationParameters.put(
         SECRET_HASH,
-        calculateSecretHash(
-          credential.getEmailAddress(), userPoolClientId, userPoolClientSecret));
+        calculateSecretHash(credential.getEmailAddress(), userPoolClientId, userPoolClientSecret));
+
     try {
       return getAuthenticationTokens(USER_PASSWORD_AUTH, authenticationParameters);
     } catch (InvalidPasswordException | NotAuthorizedException e) {
@@ -63,8 +60,9 @@ public class CognitoIdpServiceImpl implements CognitoIdpService {
 
   @Override
   public AuthToken authenticate(String refreshToken, String email) throws UnauthorisedException {
-    Map<String, String> authenticationParameters =
-      Map.of(REFRESH_TOKEN, refreshToken,
+    Map<String, String> authenticationParameters = new HashMap<>();
+    authenticationParameters.put(REFRESH_TOKEN, refreshToken);
+    authenticationParameters.put(
         SECRET_HASH, calculateSecretHash(email, userPoolClientId, userPoolClientSecret));
     try {
       return getAuthenticationTokens(REFRESH_TOKEN_AUTH, authenticationParameters);
@@ -75,15 +73,15 @@ public class CognitoIdpServiceImpl implements CognitoIdpService {
   }
 
   private AuthToken getAuthenticationTokens(
-    String authFlowType, Map<String, String> authenticationParameters) {
+      String authFlowType, Map<String, String> authenticationParameters) {
     InitiateAuthRequest authenticationRequest =
-      new InitiateAuthRequest()
-        .withAuthFlow(authFlowType)
-        .withClientId(userPoolClientId)
-        .withAuthParameters(authenticationParameters);
+        new InitiateAuthRequest()
+            .withAuthFlow(authFlowType)
+            .withClientId(userPoolClientId)
+            .withAuthParameters(authenticationParameters);
     InitiateAuthResult authenticationResult = cognitoClient.initiateAuth(authenticationRequest);
     return new AuthToken(
-      authenticationResult.getAuthenticationResult().getAccessToken(),
-      authenticationResult.getAuthenticationResult().getRefreshToken());
+        authenticationResult.getAuthenticationResult().getAccessToken(),
+        authenticationResult.getAuthenticationResult().getRefreshToken());
   }
 }
--- application/app/src/main/java/uk/nhs/digital/uec/api/service/impl/ApiUtilsService.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/service/impl/ApiUtilsService.java
@@ -1,12 +1,12 @@
 package uk.nhs.digital.uec.api.service.impl;
 
+import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
-
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -45,19 +45,22 @@ public class ApiUtilsService implements ApiUtilsServiceInterface {
   /** {@inheritDoc} */
   @Override
   public List<String> sanitiseSearchTerms(final List<String> searchCriteria) {
-
-    log.info("Sanitising search terms {}",searchCriteria);
-    List<String> listFromString = searchCriteria.stream().map(String::trim).toList();
+    log.info("Sanitising search terms {}", searchCriteria);
+    List<String> listFromString =
+        searchCriteria.stream().map(String::trim).collect(Collectors.toList());
 
     List<String> sanitisedSearchTerms = new ArrayList<>();
 
     for (String searchTerm : listFromString) {
       if (searchTerm.length() >= minSearchTermLength) {
-
         String searchTermToAdd = searchTerm.replaceAll("\\s+", " ");
 
-        //Remove URL encoding
-        searchTermToAdd = URLDecoder.decode(searchTermToAdd, StandardCharsets.UTF_8);
+        // Remove URL encoding
+        try {
+          searchTermToAdd = URLDecoder.decode(searchTermToAdd, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+          log.error(e.getMessage());
+        }
 
         // Remove weird characters
         searchTermToAdd = searchTermToAdd.replaceAll("[^a-zA-Z0-9:;.?! ]", "");
@@ -67,14 +70,14 @@ public class ApiUtilsService implements ApiUtilsServiceInterface {
         addAdditionalSearchTerms(sanitisedSearchTerms, searchTerm);
       }
     }
-    log.info("Sanitised search terms {}",sanitisedSearchTerms);
+    log.info("Sanitised search terms {}", sanitisedSearchTerms);
     return sanitisedSearchTerms;
   }
 
   /** {@inheritDoc} */
   @Override
   public String removeBlankSpaces(final String field) {
-    log.info("Formatting {}",field);
+    log.info("Formatting {}", field);
     if (field != null) {
       return field.replaceAll("\\s", "");
     }
@@ -83,12 +86,13 @@ public class ApiUtilsService implements ApiUtilsServiceInterface {
 
   @Override
   public List<String> removeBlankSpacesIn(final List<String> fields) {
-    final String POSTCODE_REGEX = "^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$";
+    final String POSTCODE_REGEX =
+        "^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$";
     Pattern pattern = Pattern.compile(POSTCODE_REGEX);
     return fields.stream()
-      .filter(field -> pattern.matcher(field).matches())
-      .map(this::removeBlankSpaces)
-      .toList();
+        .filter(field -> pattern.matcher(field).matches())
+        .map(this::removeBlankSpaces)
+        .collect(Collectors.toList());
   }
 
   /**
@@ -103,7 +107,6 @@ public class ApiUtilsService implements ApiUtilsServiceInterface {
     List<String> subSearchTerms = Arrays.asList(searchTerm.split("\\s+"));
     String previousSubSearchTerm = null;
     for (String subSearchTerm : subSearchTerms) {
-
       if (previousSubSearchTerm != null) {
         sanitisedSearchTerms.add(previousSubSearchTerm + subSearchTerm);
       }
