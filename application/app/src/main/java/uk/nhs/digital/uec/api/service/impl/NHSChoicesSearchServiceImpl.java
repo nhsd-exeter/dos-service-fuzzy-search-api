c42a78112750554d8faa182b2ebfd1344f6e7f8b
Konrad Bloor <konrad.bloor1@nhs.net>
2024-08-20 15:27:41 +0100

    More debugging of location

--- application/app/src/main/java/uk/nhs/digital/uec/api/service/impl/NHSChoicesSearchServiceImpl.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/service/impl/NHSChoicesSearchServiceImpl.java
@@ -101,7 +101,8 @@ public class NHSChoicesSearchServiceImpl implements NHSChoicesSearchService {
 
   private DosService convertNHSChoicesToDosService(
       double searchLatitude, double searchLongitude, NHSChoicesV2DataModel nhsChoicesV2DataModel) {
-    return DosService.builder()
+    log.info("Location of service {} is lat={}, long={}", nhsChoicesV2DataModel.getSearchKey(), nhsChoicesV2DataModel.getLatitude(), nhsChoicesV2DataModel.getLongitude());
+    DosService service = DosService.builder()
         ._score(servicesMapperUtil.getSearchScore(nhsChoicesV2DataModel.getSearchScore()))
         .id(nhsChoicesV2DataModel.getSearchKey())
         .u_id(nhsChoicesV2DataModel.getSearchKey())
@@ -128,5 +129,7 @@ public class NHSChoicesSearchServiceImpl implements NHSChoicesSearchService {
         .location(servicesMapperUtil.getGeoLocation(nhsChoicesV2DataModel))
         .datasource(NHS_CHOICES_DATASOURCE)
         .build();
+    log.info("Service {} has location", service.getLocation());
+    return service;
   }
 }
