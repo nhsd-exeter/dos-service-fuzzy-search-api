1c1117cbaf38d2736b6de5799894b54848dd16c4
Rohit kondapalli <rohit.kondapalli1@nhs.net>
2024-09-02 16:43:19 +0100

    SFD-6409 Disable NHS Choices due to API issue (#412)

--- application/app/src/main/java/uk/nhs/digital/uec/api/service/impl/ConcurrentFuzzySearchServiceImpl.java
+++ application/app/src/main/java/uk/nhs/digital/uec/api/service/impl/ConcurrentFuzzySearchServiceImpl.java
@@ -1,5 +1,11 @@
 package uk.nhs.digital.uec.api.service.impl;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -13,73 +19,86 @@ import uk.nhs.digital.uec.api.service.DosServiceSearch;
 import uk.nhs.digital.uec.api.service.NHSChoicesSearchService;
 import uk.nhs.digital.uec.api.util.PostcodeFormatterUtil;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 @Service
 @Slf4j
 public class ConcurrentFuzzySearchServiceImpl implements ConcurrentFuzzySearchService {
 
   private final DosServiceSearch dosSearchService;
-  private final NHSChoicesSearchService nhsChoicesSearchService;
+
+  // private final NHSChoicesSearchService nhsChoicesSearchService;
 
   @Autowired
   public ConcurrentFuzzySearchServiceImpl(
-    DosServiceSearch dosSearchService,
-    NHSChoicesSearchService nhsChoicesSearchService) {
+      DosServiceSearch dosSearchService, NHSChoicesSearchService nhsChoicesSearchService) {
     this.dosSearchService = dosSearchService;
-    this.nhsChoicesSearchService = nhsChoicesSearchService;
+    // this.nhsChoicesSearchService = nhsChoicesSearchService;
   }
 
   @Override
   @Async
-  public CompletableFuture<List<DosService>> fuzzySearch(String searchLatitude, String searchLongitude, Double distanceRange, List<String> searchTerms, String searchPostcode, Integer maxNumServicesToReturn) throws NotFoundException {
+  public CompletableFuture<List<DosService>> fuzzySearch(
+      String searchLatitude,
+      String searchLongitude,
+      Double distanceRange,
+      List<String> searchTerms,
+      String searchPostcode,
+      Integer maxNumServicesToReturn)
+      throws NotFoundException {
     log.info("Init NHS choices async call");
 
     // Format the searchPostcode using the PostcodeFormatterUtil
     String formattedPostcode = PostcodeFormatterUtil.formatPostcode(searchPostcode);
 
-    CompletableFuture<List<DosService>> nhsChoicesServicesFuture = nhsChoicesSearchService.retrieveParsedNhsChoicesV2Model(
-      searchLatitude, searchLongitude, searchTerms, formattedPostcode, maxNumServicesToReturn
-    );
+    // Commented out NHS Choices search until API issue is resolved
+    CompletableFuture<List<DosService>> nhsChoicesServicesFuture =
+        CompletableFuture.completedFuture(Collections.emptyList());
+    // nhsChoicesSearchService.retrieveParsedNhsChoicesV2Model(
+    //   searchLatitude, searchLongitude, searchTerms, formattedPostcode, maxNumServicesToReturn
+    // );
 
     log.info("Init DOS services async call");
-    CompletableFuture<List<DosService>> dosServicesFuture = CompletableFuture.supplyAsync(() -> {
-      try {
-        return dosSearchService.retrieveServicesByGeoLocation(searchLatitude, searchLongitude, distanceRange, searchTerms, formattedPostcode);
-      } catch (NotFoundException | InvalidParameterException e) {
-        log.error("Error retrieving DOS services", e);
-        throw new DosServiceSearchException("Error retrieving DOS services", e);
-      }
-    }).exceptionally(ex -> {
-      log.error("Error in DOS services search", ex);
-      return Collections.emptyList();
-    });
+    CompletableFuture<List<DosService>> dosServicesFuture =
+        CompletableFuture.supplyAsync(
+                () -> {
+                  try {
+                    return dosSearchService.retrieveServicesByGeoLocation(
+                        searchLatitude,
+                        searchLongitude,
+                        distanceRange,
+                        searchTerms,
+                        formattedPostcode);
+                  } catch (NotFoundException | InvalidParameterException e) {
+                    log.error("Error retrieving DOS services", e);
+                    throw new DosServiceSearchException("Error retrieving DOS services", e);
+                  }
+                })
+            .exceptionally(
+                ex -> {
+                  log.error("Error in DOS services search", ex);
+                  return Collections.emptyList();
+                });
 
     return CompletableFuture.allOf(dosServicesFuture, nhsChoicesServicesFuture)
-      .thenApply(ignored -> {
-        log.info("Combining results");
+        .thenApply(
+            ignored -> {
+              log.info("Combining results");
 
-        List<DosService> combinedList = Stream.concat(
-            dosServicesFuture.join().stream(),
-            nhsChoicesServicesFuture.join().stream())
-          .collect(Collectors.toList());
+              List<DosService> combinedList =
+                  Stream.concat(
+                          dosServicesFuture.join().stream(),
+                          nhsChoicesServicesFuture.join().stream())
+                      .collect(Collectors.toList());
 
-        log.info("Number of DOS Services: {}", dosServicesFuture.join().size());
-        log.info("Number of NHS Choices Services: {}", nhsChoicesServicesFuture.join().size());
+              log.info("Number of DOS Services: {}", dosServicesFuture.join().size());
+              log.info(
+                  "Number of NHS Choices Services: {}", nhsChoicesServicesFuture.join().size());
 
-        log.info("Sorting services based on distance in ascending order.");
-        combinedList.sort(Comparator.comparingDouble(DosService::getDistance));
+              log.info("Sorting services based on distance in ascending order.");
+              combinedList.sort(Comparator.comparingDouble(DosService::getDistance));
 
-        log.info("Services sorted successfully based on distance.");
+              log.info("Services sorted successfully based on distance.");
 
-        return combinedList;
-      });
+              return combinedList;
+            });
   }
-
-
 }
